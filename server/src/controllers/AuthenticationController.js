603d47bc8244538f3004cf24a535e826c73dc686
CaBra503 <CaBra503@users.noreply.github.com>
2018-05-29 21:55:39 -0700

    working on bcrypt

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -2,10 +2,10 @@ const {User} = require('../models');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
-function jwtSignUser () {
+function jwtSignUser (user) {
 	const ONE_WEEK = 60 * 60 * 24 * 7;
-	return jwt.sign(user, config.authentication.jwtSecret,{
-		exiresIn: ONE_WEEK
+	return jwt.sign(user, config.authentication.jwtSecret, {
+		expiresIn: ONE_WEEK
 	})
 }
 
@@ -13,7 +13,8 @@ module.exports = {
 	async register (req, res) {
 		try {
 			const user = await User.create(req.body)
-			res.send(user.toJSON())
+			res.send(user.toJSON());
+			console.log('user', userJson);
 		} catch (error) {
 			res.status(400).send({
 				error: 'This email account is already in use.'
@@ -22,36 +23,41 @@ module.exports = {
 	},
 	async login (req, res) {
 		try {
-			const{email, password} = req.body
+			const{email, password} = req.body;
 			const user = await User.findOne({
 				where: {
 					email: email 
 				}
-			})
-			if(!user) {
+			});
+
+			if (!user) {
+				console.log('**incorrect User information.**');
 				return res.status(403).send({
-					error: 'The Login information was incorrect.',
+					error: 'The Login information was incorrect.'
 				});
 			}
-			
-			const isPasswordValid = password === user.password;
-			if(!isPasswordValid){
-				res.status(403).send({
+			const isPasswordValid = await User.comparePassword(password);
+			if (!isPasswordValid) {
+				console.log('**incorrect Password**')
+				return res.status(403).send({
 					error: 'The Login information was incorrect.'
 				})
 			}
-			const userJson = user.toJSON();
-			console.log('user', userJson)
-			res.send({
-				user: userJson,
-				token: jwtSignUser(userJson)
-			})
-			
-		} catch (error) {
+				console.log('THIS IS PASSING THE CHECKS');
+				const userJson = user.toJSON();
+				console.log('user', userJson);
+				res.send({
+					user: userJson,
+					token: jwtSignUser(userJson)
+				});
+
+		} catch (err) {
+			console.log('**generic error**');
 			res.status(500).send({
 				error: 'An error has occured trying to log in'
-			})
+			});
 		}
+		
 	}
 	// End of login
 }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,37 @@
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
+
+function hashPassword(user, options) {
+	const SALT_FACTOR = 8;
+	
+	if (!user.changed('password')){
+		return;
+	}
+	return bcrypt.genSaltAsync(SALT_FACTOR).then(salt => bcrypt.hashAsync(user.password, salt, null)).then(hash => {
+		user.setDataValue('password', hash);
+		console.log('hashed pass');
+	});
+}
+
 //This is the User model.
-module.exports = (sequelize, DataTypes) => 
-	sequelize.define('User', {
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
 		email:{
 			type: DataTypes.STRING,
 			unique: true
 		},
 		password: DataTypes.STRING
+	},{
+		hooks: {
+			beforeCreate: hashPassword,
+			beforeUpdate: hashPassword,
+			beforeSave: hashPassword
+		}
 	});
+
+	User.prototype.comparePassword = function(password) {
+		return bcrypt.compareAsync(password, this.password);
+	};
+	
+	return User;
+};
